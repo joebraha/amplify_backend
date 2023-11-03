ba9d7fa675b8b5d872786390ac27f69e796f8f10
EC2 Default User <ec2-user@ip-172-31-3-145.us-west-1.compute.internal>
2023-11-03 02:02:39 +0000

    delete old package

--- tgenv/lib/python3.9/site-packages/tg/util/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities"""
-from .decorators import no_warn
-from .bunch import Bunch
-from .files import DottedFileNameFinder, DottedFileLocatorError
-from .lazystring import lazify, LazyString
