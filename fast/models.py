04f9d70c7821dcd9ea6210fde32323f5ebf5ea1b
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-11-27 13:58:05 -0500

    models.py update for user login

--- fast/models.py
+++ fast/models.py
@@ -3,6 +3,10 @@ from database import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func 
 
+import datetime as _dt
+import sqlalchemy as import sqlalchemy.orm as _orm
+import passlib.hash as _hash
+
 class Song(Base):
     __tablename__ = "songs"
 
@@ -23,10 +27,17 @@ class Music_Generator(Base):
 class User(Base):
     __tablename__ = "user"
 
-    user_id = Column(Integer, primary_key = True) 
-    email = Column(String)
+    user_id = Column(Integer, primary_key = True, index = True) 
+    email = Column(String, unique = True, index = True)
     library_id = Column(Integer, ForeignKey('Music_Library.user_id'))
     phone_number = Column(String)
+
+    hashed_password = Column(String)
+
+    leads = _orm.relationship("Lead", back_populates="owner")
+
+    def verify_password(self, password: str):
+        return _hash.bcrypt.verify(password, self.hashed_password)
 
 class Streaming_Service(Base):
     __tablename__ = "streaming_service"
@@ -45,3 +56,17 @@ class Music_Library(Base):
     storage_used = Column(Integer)
     user_id = Column(Integer,ForeignKey('user.user_id'), primary_key = True,)
     storage_left = Column(Integer)
+
+class Lead(_database.Base):
+    __tablename__ = "leads"
+    id = Column(Integer, primary_key=True, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, index=True)
+    company = Column(String, index=True, default="")
+    note = Column(String, default="")
+    date_created = Column(DateTime, default=_dt.datetime.utcnow)
+    date_last_updated = Column(DateTime, default=_dt.datetime.utcnow)
+
+    owner = _orm.relationship("User", back_populates="leads")
