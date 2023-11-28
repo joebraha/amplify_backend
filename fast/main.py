82672c2c6c09682221eb3535555b7190a95d6a79
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-11-28 16:12:01 -0500

    create/update user info - needs to be tested

--- fast/main.py
+++ fast/main.py
@@ -5,12 +5,9 @@ from .database import get_db
 from .models import Music_Library, Song, Music_Generator, User, Streaming_Service
 
 from typing import List
-import fastapi as _fastapi
-import fastapi.security as _security
+# import fastapi.security as _security 
 
-import sqlalchemy.orm as _orm
-
-import services as _services, schemas as _schemas
+from .schemas import UserCreate, User, Lead, LeadCreate 
 
 app = FastAPI()
 
@@ -59,70 +56,70 @@ async def post_user_playlists(user_id: int, db: Session = Depends(get_db)):
 
 @app.post("/api/users")
 async def create_user(
-    user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)):
-    db_user = await _services.get_user_by_email(user.email, db)
+    user: UserCreate, db: Session = Depends(get_db)):
+    db_user = await get_user_by_email(user.email, db)
     if db_user:
-        raise _fastapi.HTTPException(status_code=400, detail="Email already in use")
+        raise HTTPException(status_code=400, detail="Email already in use")
 
-    user = await _services.create_user(user, db)
-    return await _services.create_token(user)
+    user = await create_user(user, db)
+    return await create_token(user)
 
 
 @app.post("/api/token")
 async def generate_token(
-    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    user = await _services.authenticate_user(form_data.username, form_data.password, db)
+    form_data: _security.OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),):
+    user = await authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
 
-    return await _services.create_token(user)
+    return await create_token(user)
 
 
-@app.get("/api/users/me", response_model=_schemas.User)
-async def get_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
+@app.get("/api/users/me", response_model=User)
+async def get_user(user: User = Depends(get_current_user)):
     return user
 
 
-@app.post("/api/leads", response_model=_schemas.Lead)
+@app.post("/api/leads", response_model=Lead)
 async def create_lead(
-    lead: _schemas.LeadCreate,
-    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    return await _services.create_lead(user=user, db=db, lead=lead)
+    lead: LeadCreate,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),):
+    return await create_lead(user=user, db=db, lead=lead)
 
 
-@app.get("/api/leads", response_model=List[_schemas.Lead])
+@app.get("/api/leads", response_model=List[Lead])
 async def get_leads(
-    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    return await _services.get_leads(user=user, db=db)
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),):
+    return await get_leads(user=user, db=db)
 
 
 @app.get("/api/leads/{lead_id}", status_code=200)
 async def get_lead(
     lead_id: int,
-    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    return await _services.get_lead(lead_id, user, db)
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),):
+    return await get_lead(lead_id, user, db)
 
 
 @app.delete("/api/leads/{lead_id}", status_code=204)
 async def delete_lead(
     lead_id: int,
-    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    await _services.delete_lead(lead_id, user, db)
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),):
+    await delete_lead(lead_id, user, db)
     return {"message", "Successfully Deleted"}
 
 
 @app.put("/api/leads/{lead_id}", status_code=200)
 async def update_lead(
     lead_id: int,
-    lead: _schemas.LeadCreate,
-    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
-    db: _orm.Session = _fastapi.Depends(_services.get_db),):
-    await _services.update_lead(lead_id, lead, user, db)
+    lead: LeadCreate,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),):
+    await update_lead(lead_id, lead, user, db)
     return {"message", "Successfully Updated"}
 
 
